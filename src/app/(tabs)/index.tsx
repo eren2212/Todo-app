0a73ce295a4157bd9a47ed048ad02e1bc59c76a1
eren iridere <eiridere@gmail.com>
2025-06-14 01:44:39 +0300

    Nativewind install and setup

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500">hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
